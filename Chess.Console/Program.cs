b7dd65575a38f03036f6ac4a9438f661070ee92a
Lucas Miranda <lumidach@gmail.com>
2022-12-30 15:50:37 -0300

    Introduzida a classe Position

--- Chess.Console/Program.cs
+++ Chess.Console/Program.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using chessboard;
+using System;
 
-namespace Chess.Console
+namespace Chess
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Position board = new Position(3, 4);
+
+            Console.WriteLine("Position: " + board);
+            Console.ReadKey();
         }
     }
 }
---
+++ Chess.Console/chessboard/Position.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System;
+
+namespace chessboard
+{
+    internal class Position
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }        
+
+        public override string ToString()
+        {
+            StringBuilder toString = new StringBuilder();
+            toString.Append(Row);
+            toString.Append(", ");
+            toString.Append(Column);
+            return toString.ToString();
+        }
+    }
+}
